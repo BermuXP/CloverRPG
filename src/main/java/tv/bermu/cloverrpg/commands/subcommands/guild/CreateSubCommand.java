9ed9a509ba3d6c1c8a0b83e7482024fa00f3ac77
Bermu <bermu@bermu.tv>
2024-05-13 21:37:58 +0200

    Updated guild create command

--- src/main/java/tv/bermu/cloverrpg/commands/subcommands/guild/CreateSubCommand.java
+++ src/main/java/tv/bermu/cloverrpg/commands/subcommands/guild/CreateSubCommand.java
@@ -1,23 +1,40 @@
 package tv.bermu.cloverrpg.commands.subcommands.guild;
 
+import java.util.HashMap;
+
 import org.bukkit.entity.Player;
 
 import tv.bermu.cloverrpg.MessageFormatter;
 import tv.bermu.cloverrpg.SubCommand;
+import tv.bermu.cloverrpg.db.handlers.GuildHandler;
 
 public class CreateSubCommand implements SubCommand {
 
     private final String permission;
     private final MessageFormatter messageFormatter;
+    private final GuildHandler guildHandler;
 
-    public CreateSubCommand(MessageFormatter messageFormatter, String permission) {
+    public CreateSubCommand(GuildHandler guildHandler, MessageFormatter messageFormatter, String permission) {
         this.messageFormatter = messageFormatter;
         this.permission = permission;
+        this.guildHandler = guildHandler;
     }
 
     @Override
     public void execute(Player player, String[] args) {
+        String playerLanguage = player.getLocale().toLowerCase();
+        if (args[1] == null) {
+            HashMap<String, Object> slugs = new HashMap<>();
+            slugs.put("command_name", "guild");
+            slugs.put("subcommand", "create");
+            slugs.put("usage", "<guild_name>");
+            player.sendMessage(
+                    messageFormatter.formatMessage("subcommand_usage", playerLanguage, slugs));
+            return;
+        }
 
+        player.sendMessage(guildHandler.createGuild(args[1], player,
+                player.getLocale().toLowerCase()));
     }
 
     @Override
---
+++ src/main/java/tv/bermu/cloverrpg/db/handlers/GuildHandler.java
@@ -0,0 +1,5 @@
+package tv.bermu.cloverrpg.db.handlers;
+
+public class GuildHandler {
+    
+}
